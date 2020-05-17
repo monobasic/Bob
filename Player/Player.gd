6059b678ca2ef792a9406adbce55e8a1346bf528
André Abt <info@andre-abt.com>
2020-05-17 14:55:46 +0200

    Player state machine basics

--- Player/Player.gd
+++ Player/Player.gd
@@ -4,6 +4,13 @@ const ACCELERATION = 500
 const MAX_SPEED = 80
 const FRICTION = 500
 
+enum {
+	MOVE,
+	ROLL,
+	ATTACK
+}
+
+var state = MOVE
 var velocity = Vector2.ZERO
 
 onready var animationPlayer = $AnimationPlayer
@@ -14,6 +21,15 @@ func _ready():
 	animationTree.active = true
 
 func _physics_process(delta):
+	match state:
+		MOVE:
+			move_state(delta)
+		ROLL:
+			pass
+		ATTACK:
+			attack_state(delta)
+	
+func move_state(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right")	- Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up") 
@@ -29,3 +45,7 @@ func _physics_process(delta):
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 
 	velocity = move_and_slide(velocity)
+
+
+func attack_state(delta):
+	pass
