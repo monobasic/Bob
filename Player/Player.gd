0ed19ebbfc57b219486023fbaefcfae613afb206
André Abt <info@andre-abt.com>
2020-05-17 14:41:31 +0200

    Activate animationTree by script

--- Player/Player.gd
+++ Player/Player.gd
@@ -10,13 +10,14 @@ onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get("parameters/playback")
 
+func _ready():
+	animationTree.active = true
+
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right")	- Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up") 
 	input_vector = input_vector.normalized()
-	
-	print(input_vector)
 	
 	if input_vector != Vector2.ZERO:
 		animationTree.set("parameters/Idle/blend_position", input_vector)
