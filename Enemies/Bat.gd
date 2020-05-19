fac20b3b0567b1afcff5e60bbca9c856b6fe29ac
André Abt <info@andre-abt.com>
2020-05-19 07:38:39 +0200

    Enemy AI basics

--- Enemies/Bat.gd
+++ Enemies/Bat.gd
@@ -2,13 +2,49 @@ extends KinematicBody2D
 
 const EnemyDeathEffect = preload("res://Effects/EnemyDeathEffect.tscn")
 
+export var ACCELERATION = 300
+export var MAX_SPEED = 50
+export var FRICTION = 200
+
+enum {
+	IDLE,
+	WANDER,
+	CHASE
+}
+
+var state = IDLE
+
+var velocity = Vector2.ZERO
 var knockback = Vector2.ZERO
 
+onready var animatedSprite = $AnimatedSprite
 onready var stats = $Stats
+onready var playerDetectionZone = $PlayerDetectionZone
 
 func _physics_process(delta):
-	knockback = knockback.move_toward(Vector2.ZERO, 200 * delta) # Friction
+	knockback = knockback.move_toward(Vector2.ZERO, FRICTION * delta) # Friction
 	knockback = move_and_slide(knockback)
+	
+	match state:
+		IDLE:
+			velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+			seek_player()
+		WANDER:
+			pass
+		CHASE:
+			var player = playerDetectionZone.player
+			if player != null:
+				# move towards the player
+				var direction = (player.global_position - global_position).normalized()
+				velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
+				animatedSprite.flip_h = velocity.x < 0
+			else:
+				state = IDLE
+	velocity = move_and_slide(velocity)
+
+func seek_player():
+	if playerDetectionZone.can_see_player():
+		state = CHASE
 
 func _on_Hurtbox_area_entered(area):
 	stats.health -= area.damage
