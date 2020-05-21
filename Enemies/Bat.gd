d3320f0e6b17f067015721f8f7949ea38b67a593
André Abt <info@andre-abt.com>
2020-05-21 21:43:23 +0200

    Use new hurtboxes

--- Enemies/Bat.gd
+++ Enemies/Bat.gd
@@ -20,6 +20,7 @@ var knockback = Vector2.ZERO
 onready var animatedSprite = $AnimatedSprite
 onready var stats = $Stats
 onready var playerDetectionZone = $PlayerDetectionZone
+onready var hurtbox = $Hurtbox
 
 func _physics_process(delta):
 	knockback = knockback.move_toward(Vector2.ZERO, FRICTION * delta) # Friction
@@ -49,6 +50,7 @@ func seek_player():
 func _on_Hurtbox_area_entered(area):
 	stats.health -= area.damage
 	knockback = area.knockback_vector * 120
+	hurtbox.create_hit_effect()
 
 func _on_Stats_no_health():
 	queue_free()
