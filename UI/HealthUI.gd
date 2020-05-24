26f98b7e87799dd72df6d416dcc853c57229eee9
André Abt <info@andre-abt.com>
2020-05-24 10:20:17 +0200

    Heart UI

--- UI/HealthUI.gd
+++ UI/HealthUI.gd
@@ -3,15 +3,18 @@ extends Control
 var hearts = 4 setget set_hearts
 var max_hearts = 4 setget set_max_hearts
 
-onready var label = $Label
+onready var heartUIFull = $HeartUIFull
+onready var heartUIEmpty = $HeartUIEmpty
 
 func set_hearts(value):
 	hearts = clamp(value, 0, max_hearts)
-	if label != null:
-		label.text = "HP = " + str(hearts)
+	if heartUIFull != null:
+		heartUIFull.rect_size.x = hearts * 15
 	
 func set_max_hearts(value):
 	max_hearts = max(value, 1)
+	if heartUIEmpty != null:
+		heartUIEmpty.rect_size.x = max_hearts * 15
 
 func _ready():
 	max_hearts = PlayerStats.max_health
