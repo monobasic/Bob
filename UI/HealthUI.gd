6e1442569f967cf9a2dcd6ad8c3e789fd2208cf0
André Abt <info@andre-abt.com>
2020-05-24 10:33:33 +0200

    Set max_health on ready

--- UI/HealthUI.gd
+++ UI/HealthUI.gd
@@ -6,18 +6,21 @@ var max_hearts = 4 setget set_max_hearts
 onready var heartUIFull = $HeartUIFull
 onready var heartUIEmpty = $HeartUIEmpty
 
+func _ready():
+	PlayerStats.connect("health_changed", self, "set_hearts")
+	PlayerStats.connect("max_health_changed", self, "set_max_hearts")
+	self.max_hearts = PlayerStats.max_health
+	self.hearts = PlayerStats.health	
+
 func set_hearts(value):
 	hearts = clamp(value, 0, max_hearts)
 	if heartUIFull != null:
 		heartUIFull.rect_size.x = hearts * 15
 	
 func set_max_hearts(value):
+	print("set_max_hearts")
 	max_hearts = max(value, 1)
 	if heartUIEmpty != null:
 		heartUIEmpty.rect_size.x = max_hearts * 15
 
-func _ready():
-	max_hearts = PlayerStats.max_health
-	self.hearts = PlayerStats.health
-	PlayerStats.connect("health_changed", self, "set_hearts")
-	PlayerStats.connect("max_health_changed", self, "set_max_hearts")
+
