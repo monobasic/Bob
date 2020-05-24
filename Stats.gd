36c96f4abf7b1a22b81a511d102c506f20fa8cac
André Abt <info@andre-abt.com>
2020-05-24 10:20:25 +0200

    Max health setter

--- Stats.gd
+++ Stats.gd
@@ -1,9 +1,10 @@
 extends Node
 
-export(int) var max_health = 1
+export(int) var max_health = 1 setget set_max_health
 onready var health = max_health setget set_health
 
 signal health_changed(value)
+signal max_health_changed(value)
 signal no_health
 
 
@@ -12,3 +13,8 @@ func set_health(value):
 	emit_signal("health_changed", health)
 	if health <= 0:
 		emit_signal("no_health")
+		
+func set_max_health(value):
+	max_health =  value
+	emit_signal("max_health_changed", max_health)
+	
